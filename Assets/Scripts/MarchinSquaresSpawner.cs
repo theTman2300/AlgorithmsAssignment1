a497bba47fe34ad159f446e5324ab70e544d3445
Tijmen Prins <prinstijmen@gmail.com>
2025-04-16 22:24:42 +0200

    Started marching squares

--- Assets/Scripts/MarchinSquaresSpawner.cs
+++ Assets/Scripts/MarchinSquaresSpawner.cs
@@ -1,9 +1,11 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 [RequireComponent(typeof(TilemapGenerator))]
 public class MarchinSquaresSpawner : MonoBehaviour
 {
     TilemapGenerator tilemapGenerator;
+    int[,] tilemap;
     int[,] marchedTilemap;
 
     void Start()
@@ -11,5 +13,17 @@ public class MarchinSquaresSpawner : MonoBehaviour
         tilemapGenerator = GetComponent<TilemapGenerator>();
     }
 
-    
+    [Button]
+    void CreateMarchingSquaresTilemap()
+    {
+        tilemap = tilemapGenerator.GetTilemap();
+
+        for (int x = 0; x < tilemap.GetLength(0) - 2; x++)
+        {
+            for (int y = 0; y < tilemap.GetLength(1) - 2; y++)
+            {
+
+            }
+        }
+    }
 }
